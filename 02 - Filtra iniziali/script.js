17186270189586b75844a97cbf59c3de780e9d22
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-21 15:45:02 +0200

    2 esercizio

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,12 +1,25 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+let newArray = []
+let word = prompt("Inserisci una lettera")
 
 
 // Dichiara la funzione qui.
+const findname = (a) => {
 
+  for (i = 0; i < names.length; i++) {
+      let newEl = names[i].charAt(0)
+
+      if (word === newEl){
+        newArray.push(names[i])
+      }       
+    }
+  return newArray
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log (findname())
 
 
 

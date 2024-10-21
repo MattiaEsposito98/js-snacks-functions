301b850a99b19cbf24700f33ae63ee9a906e2b21
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-21 16:29:58 +0200

    4 esercizio

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,12 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto(word) {
+return "Ciao " + name
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluto(name))
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario

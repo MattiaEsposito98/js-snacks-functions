9cff21a0414dfc10f64fa227cb873ae392776c19
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-21 16:12:18 +0200

    3 esercizio

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,28 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+const word = 'javascript'
+let res= ''
+let count = ''
 
 
 // Dichiara la funzione qui.
+const countVowels = (vowels) => {
 
+  for (i = 0; i < word.length; i++) {
+    let newEl = word[i]
+    console.log (newEl)
+  
+    if (newEl === "a" || newEl === "e" || newEl === "i" || newEl === "o" || newEl === "u") {
+      count += newEl
+      res += newEl + ','
+    }
+  }
+  return res
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log (countVowels())
+console.log (count.length)
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
